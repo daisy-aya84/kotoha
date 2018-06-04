38302ce2ce93f6adbbea3940c1040447ba2ba500
Ayano_Masumoto <dg.lw.8aya4@gmail.com>
2018-06-04 02:19:51 +0000

    ログインログアウト機能完成！

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,8 @@
 class UserController < ApplicationController
+  before_action :authenticate_user,{only: [:index, :show, :edit, :update]}
+  before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
+  before_action :ensure_correct_user, {only: [:edit, :update]}
+  
   def index
     @users = User.all
   end
@@ -66,5 +70,12 @@ class UserController < ApplicationController
     session[:user_id] = nil
     flash[:notice] = "ログアウトしました"
     redirect_to("/login")
+  end
+  
+  def ensure_correct_user
+    if @current_user.id != params[:id].to_i
+      flash[:notice] = "権限がありません"
+      redirect_to("/post/index")
+    end
   end
 end
